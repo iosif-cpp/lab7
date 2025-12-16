8a4908caf6fab47b42ba9724cdd6c65b31045a99
Joseph <osya.amer@gmail.com>
2025-12-16 18:05:40 +0300

    upd main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, useLocation, Navigate } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Transactions from './components/Transactions'
 import AddTransaction from './components/AddTransaction'
@@ -130,6 +130,7 @@ function App() {
                 path="/analytics" 
                 element={<Analytics transactions={transactions} />} 
               />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
         </div>
