089bf2a1177db33f5677492204c15c9db3925c24
Joseph <osya.amer@gmail.com>
2025-12-16 18:01:06 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // замените 'lab7' на имя вашего репозитория, если оно другое
+  base: '/lab7/',
 })
 
 
